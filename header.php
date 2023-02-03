f9b79ece456ec394d7439bddcd7cdcaf73935f09
Luca Ruggeri <lucaruggeri18@gmail.com>
2023-02-03 09:30:16 -0500

    s2c2 création et modification de scss pour pied de page et links

--- header.php
+++ header.php
@@ -8,9 +8,6 @@
 </head>
 <body>
     <header>
-        <h3>-<?php  echo bloginfo('name') ?></h3>
-        <h3>-<?php  echo bloginfo('description') ?></h3>
-        <h3>-<?php  echo bloginfo('url') ?></h3>
         <h1><a href="<?php  echo bloginfo('url') ?>"><?php  echo bloginfo('name') ?></a></h1>
         <h2><?php  echo bloginfo('description') ?></h2>
     </header>
