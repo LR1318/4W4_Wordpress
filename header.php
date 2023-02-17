0f5e0ed37c92b4c0dfadfde8982017d490a72c8c
Luca Ruggeri <lucaruggeri18@gmail.com>
2023-02-17 11:07:40 -0500

    s2c2 avancement changement scss avec flex/grid

--- header.php
+++ header.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 
@@ -16,6 +19,9 @@
         "container" => "nav"
         )) ?>
     </section>
-    <h1><a href="<?= bloginfo('url') ?>"><?= bloginfo('name') ?></a></h1>
-    <h2><?= bloginfo('description') ?></h2>
+    <section id="site__header__titre">
+
+        <h1><a href="<?= bloginfo('url') ?>"><?= bloginfo('name') ?></a></h1>
+        <h2><?= bloginfo('description') ?></h2>
+    </section>
    </header>
--- readme.md
+++ readme.md
@@ -1,14 +1,8 @@
-# Exercice #1
+# Lab 3
 ## 4w4 - Conception d'interface et développement Web
-### Auteur: Eddy Martin
+### Auteur: Luca Ruggeri
 #### Description
-- Installation sur un serveur distant « siteground.com »
-- Développer les modules suivants:
-> header.php, front-page.php
-- Création de la branche exer1 
-- Compléter la codification sass
-- Améliorer le design global
-
+- Mardown
 #### Références
 - https://eddytuto.ca/
 - https://developer.wordpress.org/themes/
