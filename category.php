08269c816278948e0c03523d6d655c2fe9aa068a
Luca Ruggeri <lucaruggeri18@gmail.com>
2023-02-24 11:01:12 -0500

    s5c2 modification lien recherche et markdown css

--- category.php
+++ category.php
@@ -5,24 +5,26 @@
  */
 ?>
 <?php get_header(); ?>
-<main>
-    <h3>categorie.php</h3>
-    <section class="blocflex">
-    <?php
-        if (have_posts()): 
-            while (have_posts()) : the_post(); ?>
-            <article>
-                <h2>
-                    <a href="<?php echo get_permalink();?>"><?php echo get_the_title(); ?></a>
-                </h2>
-                <?php // the_content();  // affiche le contenu complet de l'article ?>
-                <?php // the_excerpt();   // affiche un résumé de l'article ?>
-                <p><?= wp_trim_words(get_the_excerpt(), 10, "&#10148;") ?></p>
-            </article>   
-            <?php endwhile;
-        endif;    
-    ?>
-    </section>
+<main class="site__main">
+   <section class="blocflex">
+      <?php
+      $category = get_queried_object();
+      $args = array(
+         'category_name' => $category->slug,
+         'orderby' => 'title',
+         'order' => 'ASC'
+      );
+      $query = new WP_Query( $args );
+      if ( $query->have_posts() ) :
+         while ( $query->have_posts() ) : $query->the_post(); ?>
+            <article class="enfant_machin">
+               <h4><a href="<?php the_permalink(); ?>"> <?= get_the_title(); ?></a></h4>
+               <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
+            </article>
+         <?php endwhile; ?>
+      <?php endif;
+      wp_reset_postdata();?>
+   </section>
 </main>
 
 <?php get_footer(); ?>
--- search.php
+++ search.php
@@ -10,12 +10,20 @@
 <h3>Resultat de la recherche</h3>
 <?php
     if (have_posts()): 
-        while (have_posts()) : the_post();
-            the_title('<h2>','</h2>');
+        while (have_posts()) : the_post();?>
+        <section class="chaque_recherche">
+            
+            
+            <h4><a href="<?php the_permalink(); ?>"> <?= get_the_title(''); ?></a></h4>
+            <div class="contenu_recherche">
+                
+                <?php
             //the_content();
             echo wp_trim_words(get_the_excerpt(), 50, "[...]");
             
             ?>
+            </div>
+        </section>
             <hr>
         <?php endwhile;
     endif;
