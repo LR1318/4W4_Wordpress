87be37a47f70b81ca38404c633f928ef3aa3deb0
Luca Ruggeri <lucaruggeri18@gmail.com>
2023-04-21 10:28:55 -0400

    s12 c2 début des widgets

--- footer.php
+++ footer.php
@@ -1,12 +1,19 @@
 <footer class="site__footer">
     <section><div class="logomenu">
                <?php the_custom_logo(); ?>
-            </div></section>
+            </div>
+            <div class="widget2">
+    <?php dynamic_sidebar( 'widget2' ); ?>
+</div></section>
     <section id="menu__bas_de_page"><?php wp_nav_menu(array(
         "menu" => "entete",
         "container" => "nav"
         )) ?></section>
-    
+    <section>
+    <div class="footer">
+    <?php dynamic_sidebar( 'footer' ); ?>
+</div>
+    </section>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- functions.php
+++ functions.php
@@ -61,3 +61,27 @@ function cidweb_modifie_requete_principal( $query ) {
       }
      }
      add_action( 'pre_get_posts', 'cidweb_modifie_requete_principal' );
+
+
+     //----------------------------------------- Enregistrer le sidebar
+function enregistrer_sidebar() {
+    register_sidebar( array(
+        'name' => __( 'Footer', '4w4_Luca_Ruggeri' ),
+        'id' => 'footer',
+        'description' => __( 'Un widget area pour afficher des widgets dans la footer.', '4w4_Luca_Ruggeri' ),
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="widget-title">',
+        'after_title' => '</h2>',
+    ) );
+    register_sidebar( array(
+        'name' => __( 'widget2', '4w4_Luca_Ruggeri' ),
+        'id' => 'widget2',
+        'description' => __( 'Un widget area pour afficher des widgets', '4w4_Luca_Ruggeri' ),
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="widget-title">',
+        'after_title' => '</h2>',
+    ) );
+}
+add_action( 'widgets_init', 'enregistrer_sidebar' );
