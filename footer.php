e0c3b45ca4b42707a858b42c384853b12c6642d7
Luca Ruggeri <lucaruggeri18@gmail.com>
2023-04-24 02:13:45 -0400

    s12 c2 début organisation en colonne u footer avec 2 widget

--- footer.php
+++ footer.php
@@ -1,22 +1,25 @@
 <footer class="site__footer">
-    <section>
-        
-    </section>
-    <section><div class="logomenu">
-               <?php the_custom_logo(); ?>
-            </div>
-            <div class="widget2">
-    <?php dynamic_sidebar( 'widget2' ); ?>
-</div></section>
-    <section id="menu__bas_de_page"><?php wp_nav_menu(array(
-        "menu" => "entete",
-        "container" => "nav"
-        )) ?></section>
-    <section>
-    <div class="footer">
-    <?php dynamic_sidebar( 'footer' ); ?>
-</div>
-    </section>
+    <section class="colonne1">
+
+        <section><div class="logomenu">
+            <?php the_custom_logo(); ?>
+        </div>
+        <div class="widget2">
+            <?php dynamic_sidebar( 'widget2' ); ?>
+        </div></section>
+        <section id="menu__bas_de_page"><?php wp_nav_menu(array(
+            "menu" => "entete",
+            "container" => "nav"
+            )) ?></section>
+        </section>
+        <section class="colonne2">
+
+            <section>
+                <div class="footer">
+                    <?php dynamic_sidebar( 'footer' ); ?>
+                </div>
+            </section>
+        </section>
 </footer>
 <?php wp_footer(); ?>
 </body>
